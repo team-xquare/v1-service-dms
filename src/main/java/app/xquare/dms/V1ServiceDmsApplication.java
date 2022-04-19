b7430e8e796c7ea5404ace269dceadea12cf6295
정지우 <jiwoourty@gmail.com>
2022-04-19 15:10:38 +0900

    :gear: :: project init

---
+++ src/main/java/app/xquare/dms/V1ServiceDmsApplication.java
@@ -0,0 +1,13 @@
+package app.xquare.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1ServiceDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V1ServiceDmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/xquare/dms/XquareV1ServiceDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.xquare.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XquareV1ServiceDmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
