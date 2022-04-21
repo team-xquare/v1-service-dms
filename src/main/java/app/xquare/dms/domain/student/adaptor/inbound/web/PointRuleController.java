4660ba8c9246b980897b4fa8516d5f4dd1431b6d
정지우 <jiwoourty@gmail.com>
2022-04-22 08:53:10 +0900

    ⚡️ :: (#4) point rule controller

---
+++ src/main/java/app/xquare/dms/domain/student/adaptor/inbound/web/PointRuleController.java
@@ -0,0 +1,11 @@
+package app.xquare.dms.domain.student.adaptor.inbound.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/points/rule")
+public class PointRuleController {
+}
