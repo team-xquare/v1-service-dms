3a439246cce74ffeaf18a238e13f18fcd9e5983d
정지우 <jiwoourty@gmail.com>
2022-04-21 23:09:16 +0900

    ⚡️ :: (#2) implement usecase

--- src/main/java/app/xquare/dms/domain/student/application/service/StudentService.java
+++ src/main/java/app/xquare/dms/domain/student/application/service/StudentService.java
@@ -2,15 +2,25 @@ package app.xquare.dms.domain.student.application.service;
 
 import app.xquare.dms.domain.student.application.port.inbound.GetStudentListUseCase;
 import app.xquare.dms.domain.student.application.port.inbound.dto.StudentListResponse;
+import app.xquare.dms.domain.student.application.port.outbound.FindStudentPort;
+import app.xquare.dms.domain.student.domain.Student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StudentService implements GetStudentListUseCase {
 
+    private final FindStudentPort findStudentPort;
+
     @Override
     public StudentListResponse getStudentList() {
-        return null;
+        List<Student> students = findStudentPort.findStudent();
+
+        return StudentListResponse.builder()
+                .students(students)
+                .build();
     }
 }
