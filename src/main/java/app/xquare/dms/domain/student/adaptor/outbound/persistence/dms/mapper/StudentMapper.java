ed33d575e5c23bfff1bfc8d3d29bdf3c7876c32f
정지우 <jiwoourty@gmail.com>
2022-04-25 12:48:29 +0900

    :bookmark_tabs: :: (#12) isPenaltyRequired property

--- src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/mapper/StudentMapper.java
+++ src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/mapper/StudentMapper.java
@@ -17,7 +17,7 @@ public class StudentMapper {
                 .goodPoint(pointStatus.getGoodPoint())
                 .badPoint(pointStatus.getBadPoint())
                 .penaltyLevel(pointStatus.getPenaltyLevel())
-                .isPenaltyDone(pointStatus.getPenaltyStatus())
+                .isPenaltyRequired(pointStatus.getPenaltyStatus())
                 .build();
     }
 
