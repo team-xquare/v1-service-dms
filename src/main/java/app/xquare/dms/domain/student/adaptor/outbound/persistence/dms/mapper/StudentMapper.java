41571e4aaac4eadb1b8dcba4893d586d97d1479b
정지우 <jiwoourty@gmail.com>
2022-05-24 16:04:05 +0900

    ⚡️ :: (#28) modify mapper, apdaptor

--- src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/mapper/StudentMapper.java
+++ src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/mapper/StudentMapper.java
@@ -2,6 +2,7 @@ package app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.mapper;
 
 import app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.entity.PointHistoryJpaEntity;
 import app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.entity.PointStatusJpaEntity;
+import app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.entity.StudentJpaEntity;
 import app.xquare.dms.domain.student.domain.PointHistory;
 import app.xquare.dms.domain.student.domain.Student;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,20 @@ public class StudentMapper {
                 .badPoint(pointStatus.getBadPoint())
                 .penaltyLevel(pointStatus.getPenaltyLevel())
                 .isPenaltyRequired(pointStatus.getPenaltyStatus())
+                .build();
+    }
+
+    public Student mapToStudent(StudentJpaEntity student) {
+        return Student.builder()
+                .id(student.getId())
+                .name(student.getName())
+                .num(student.getNumber())
+                .goodPoint(student.getPointStatus().getGoodPoint())
+                .badPoint(student.getPointStatus().getBadPoint())
+                .penaltyLevel(student.getPointStatus().getPenaltyLevel())
+                .isPenaltyRequired(student.getPointStatus().getPenaltyStatus())
+                .stayApply(mapStayApplyValue(student.getStayApply().getValue()))
+                .mealApply(mapMealApplyValue(student.getMealApply().getValue()))
                 .build();
     }
 
@@ -41,5 +56,33 @@ public class StudentMapper {
                 .point(pointHistory.getPoint().getPoint())
                 .pointType(pointHistory.getPoint().getType())
                 .build();
+    }
+
+    private String mapStayApplyValue(Integer value) {
+        switch (value) {
+            case 1:
+                return "금요귀가";
+            case 2:
+                return "토요귀가";
+            case 3:
+                return "토요귀사";
+            case 4:
+                return "잔류";
+            default:
+                return null;
+        }
+    }
+
+    private String mapMealApplyValue(Integer value) {
+        switch (value) {
+            case 1:
+                return "대기";
+            case 2:
+                return "신청";
+            case 3:
+                return "미신청";
+            default:
+                return null;
+        }
     }
 }
--- src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/repository/StudentRepository.java
+++ src/main/java/app/xquare/dms/domain/student/adaptor/outbound/persistence/dms/repository/StudentRepository.java
@@ -1,9 +1,15 @@
 package app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.repository;
 
 import app.xquare.dms.domain.student.adaptor.outbound.persistence.dms.entity.StudentJpaEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends CrudRepository<StudentJpaEntity, String> {
+
+    @Query("select s from StudentJpaEntity s join fetch s.pointStatus join fetch s.stayApply join fetch s.mealApply order by s.number")
+    public List<StudentJpaEntity> findAllOrderByStudentNumber();
 }
