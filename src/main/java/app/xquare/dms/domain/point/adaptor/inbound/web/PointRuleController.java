4d41502d28775b30f3a6b7c9f42226a0da5b3013
정지우 <jiwoourty@gmail.com>
2022-06-02 10:01:45 +0900

    ⚡️ :: (#30) create point rule service

--- src/main/java/app/xquare/dms/domain/point/adaptor/inbound/web/PointRuleController.java
+++ src/main/java/app/xquare/dms/domain/point/adaptor/inbound/web/PointRuleController.java
@@ -1,11 +1,13 @@
 package app.xquare.dms.domain.point.adaptor.inbound.web;
 
+import app.xquare.dms.domain.point.application.port.inbound.CreatePointRuleUseCase;
 import app.xquare.dms.domain.point.application.port.inbound.GetPointListUseCase;
+import app.xquare.dms.domain.point.application.port.inbound.dto.request.PointRuleRequest;
 import app.xquare.dms.domain.point.application.port.inbound.dto.response.PointListResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PointRuleController {
 
     private final GetPointListUseCase getPointListUseCase;
+    private final CreatePointRuleUseCase createPointRuleUseCase;
 
     @GetMapping
     public PointListResponse pointList() {
         return getPointListUseCase.getPointList();
+    }
+
+    @PostMapping
+    public void pointRule(@Valid @RequestBody PointRuleRequest request) {
+        createPointRuleUseCase.createPointRule(request);
     }
 }
--- src/main/java/app/xquare/dms/domain/point/application/service/PointService.java
+++ src/main/java/app/xquare/dms/domain/point/application/service/PointService.java
@@ -1,19 +1,24 @@
 package app.xquare.dms.domain.point.application.service;
 
+import app.xquare.dms.domain.point.application.port.inbound.CreatePointRuleUseCase;
 import app.xquare.dms.domain.point.application.port.inbound.GetPointListUseCase;
+import app.xquare.dms.domain.point.application.port.inbound.dto.request.PointRuleRequest;
 import app.xquare.dms.domain.point.application.port.inbound.dto.response.PointListResponse;
 import app.xquare.dms.domain.point.application.port.outbound.FindPointPort;
+import app.xquare.dms.domain.point.application.port.outbound.SavePointPort;
 import app.xquare.dms.domain.point.domain.Point;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class PointService implements GetPointListUseCase {
+public class PointService implements GetPointListUseCase, CreatePointRuleUseCase {
 
     private final FindPointPort findPointPort;
+    private final SavePointPort savePointPort;
 
     @Override
     public PointListResponse getPointList() {
@@ -22,5 +27,17 @@ public class PointService implements GetPointListUseCase {
         return PointListResponse.builder()
                 .rules(points)
                 .build();
+    }
+
+    @Transactional
+    @Override
+    public void createPointRule(PointRuleRequest request) {
+        Point point = Point.builder()
+                .point(request.getPoint())
+                .reason(request.getReason())
+                .type(request.getType())
+                .build();
+
+        savePointPort.savePoint(point);
     }
 }
